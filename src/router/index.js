539e00ff01bf411a426e4c12a87789abc301aac5
vue-hsuan <useforvue@gmail.com>
2021-10-30 11:31:38 +0800

    1

--- src/router/index.js
+++ src/router/index.js
@@ -15,6 +15,14 @@ const routes = [
         // this generates a separate chunk (about.[hash].js) for this route
         // which is lazy-loaded when the route is visited.
         component: () => import('../views/About.vue'),
+        children: [
+            {
+                // UserProfile will be rendered inside User's <router-view>
+                // when /user/:id/profile is matched
+                path: 'profile',
+                component: () => import('@/components/Profile.vue'),
+            },
+        ],
     },
 ]
 
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,6 @@
 <template>
     <div class="about">
         <h1>vue hsuan 台中市 牛排</h1>
+        <router-view></router-view>
     </div>
 </template>
